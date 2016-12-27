3e4cc5377b264ee7ec1c2c4afd00d886078d1d4f
Paul <paul.w.edlund.ctr@mail.mil>
2016-12-27 16:10:53 -0500

    Added Equipment request and record service calls and fake data

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -6,7 +6,8 @@ import { NotificationService } from './supply/notification-service';
 import { OrderService } from './supply/order-service';
 import { IssueService } from './supply/issue-service';
 import { SupplyItemService } from './supply/supply-item-service';
-
+import { EquipmentRequestService } from "./supply/equipment-request-service";
+import { EquipmentRecordService } from './supply/equipment-record-service';
 
 
 @NgModule({
@@ -17,7 +18,9 @@ import { SupplyItemService } from './supply/supply-item-service';
         NotificationService,
         OrderService,
         SupplyItemService,
-        IssueService
+        IssueService,
+        EquipmentRequestService,
+        EquipmentRecordService
     ]
 })
 export class ServicesModule {
--- src/app/views/equipment/requests/equipment-requests.component.ts
+++ src/app/views/equipment/requests/equipment-requests.component.ts
@@ -3,30 +3,47 @@ import {NavController, NavParams} from 'ionic-angular';
 
 import {EquipmentRequestDetailsComponent} from './details/equip-request-details.component';
 
+import { EquipmentRequestModel } from '../../../models/equipment-request.model';
+import { EquipmentRequestService } from "../../../services/supply/equipment-request-service";
+
 @Component({
     templateUrl: 'equipment-requests.component.html'
 })
 
 export class EquipmentRequestsComponent {
-    selectedItem: any;
-    icons: string[];
-    items: Array<{title: string, note: string, icon: string}>;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    public equipmentRequestList: Array<EquipmentRequestModel>;
+
+    selectedItem: any;
+    // icons: string[];
+    // items: Array<{title: string, note: string, icon: string}>;
+
+    constructor(public navCtrl: NavController, 
+        public navParams: NavParams, 
+        public equipmentRequestService: EquipmentRequestService) {
         // If we navigated to this page, we will have an item available as a nav param
         this.selectedItem = navParams.get('item');
 
-        this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-            'american-football', 'boat', 'bluetooth', 'build'];
+        // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+        //     'american-football', 'boat', 'bluetooth', 'build'];
 
-        this.items = [];
-        for (let i = 1; i < 11; i++) {
-            this.items.push({
-                title: 'Equipment Request ' + i,
-                note: 'This is Equipment Request #' + i,
-                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-            });
-        }
+        // this.items = [];
+        // for (let i = 1; i < 11; i++) {
+        //     this.items.push({
+        //         title: 'Equipment Request ' + i,
+        //         note: 'This is Equipment Request #' + i,
+        //         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        //     });
+        // }
+    }
+
+    getAllEquipmentRequests() {
+      this.equipmentRequestService.getAllRequests().then(requests => this.equipmentRequestList = requests);
+    }
+
+    ionViewWillEnter() {
+        this.getAllEquipmentRequests();       
+
     }
 
     itemTapped(item) {

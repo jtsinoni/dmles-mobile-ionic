098b4dc2dab244635b66afc763c5de99a16f6b3c
John Tsinonis <jtsinoni@comcast.net>
2017-01-13 16:11:42 -0500

    added LoggerService to RequestApiService

--- src/app/common/endpoints/request-api.service.ts
+++ src/app/common/endpoints/request-api.service.ts
@@ -1,17 +1,17 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
-import {Logger} from "angular2-logger/core";
 import {Observable} from "rxjs";
 
 import {ApiService} from "../../services/api.service";
 import {AppService} from "../../services/app.service";
 import {AuthenticationService} from "../../services/authentication.service";
+import {LoggerService} from "../../services/logger/logger-service";
 
 @Injectable()
 export class RequestApiService extends ApiService {
 
     constructor(http: Http,
-                public log: Logger,
+                public log: LoggerService,
                 protected authenticationService: AuthenticationService,
                 private app: AppService) {
         super(http, log, authenticationService, app, "EquipmentManagement");

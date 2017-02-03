c337f7386f3768e108e528c782628c98572a676b
Mike Campbell <michael.e.campbell82.ctr@mail.mil>
2017-02-03 06:43:35 -0500

    added servername to list

--- src/app/configs/app-config.ts
+++ src/app/configs/app-config.ts
@@ -10,7 +10,9 @@ export class AppConfig {
         //"btBaseUrl"   : "http://140.139.224.44:8080/"  // Test
 
         //"btBaseUrl"   : "http://jw8dmles102:8080/"  // Dev Old
-        //"btBaseUrl"   : "http://140.139.227.170:8080/"  // Dev Old
+        //"btBaseUrl"   : "http://140.139.227.170:8080/"  // Dev Old - jw8dmles103 (DON'T USE)
+        //"btBaseUrl"   : "http://140.139.227.169:8080/"  // Dev Old - jw9dmles102 (USE)
+
 
         //"btBaseUrl"   : "https:/140.139.35.29:8443/"  // Dev New
         "btBaseUrl": "http://localhost:8080/"

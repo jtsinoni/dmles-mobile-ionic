f1ee8a2799ca631180e45c1a2e70421fd39ec6a6
John Tsinonis <jtsinoni@comcast.net>
2017-02-08 13:12:32 -0500

    commented out server-selector.component.ts prod switch would fail build

--- src/app/views/utilities/server-selector.component.ts
+++ src/app/views/utilities/server-selector.component.ts
@@ -1,30 +1,30 @@
-import { Component } from '@angular/core';
-import {ServerModel} from '../../models/server.model';
-
-@Component({
-  selector: 'server-selector',
-  templateUrl: './server-selector.html'
-})
-export class ServerSelectorComponent {
-
-  servers: Array<ServerModel>;
-
-  constructor() {
-      // get servers from db
-      this.servers = new Array<ServerModel>();
-  }
-
-  public addServer(server: ServerModel) {
-    //add to db
-  }
-
-  public removeServer(server: ServerModel) {
-    // remove from db
-  }
-
-  public selectServer(server: ServerModel) {
-    // set as default
-  }
-
-
-}
+// import { Component } from '@angular/core';
+// import {ServerModel} from '../../models/server.model';
+//
+// @Component({
+//   selector: 'server-selector',
+//   templateUrl: './server-selector.html'
+// })
+// export class ServerSelectorComponent {
+//
+//   servers: Array<ServerModel>;
+//
+//   constructor() {
+//       // get servers from db
+//       this.servers = new Array<ServerModel>();
+//   }
+//
+//   public addServer(server: ServerModel) {
+//     //add to db
+//   }
+//
+//   public removeServer(server: ServerModel) {
+//     // remove from db
+//   }
+//
+//   public selectServer(server: ServerModel) {
+//     // set as default
+//   }
+//
+//
+// }

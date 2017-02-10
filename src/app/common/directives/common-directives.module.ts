6e7f7c23d6de23264fac7d565680a0501738e503
Mike Campbell <michael.e.campbell82.ctr@mail.mil>
2017-02-10 09:25:32 -0500

    added growl

--- src/app/common/directives/common-directives.module.ts
+++ src/app/common/directives/common-directives.module.ts
@@ -6,16 +6,17 @@ import {RemoveParentElementDirective} from './remove-parent-element.directive';
 import {HeaderPageComponent} from "../../views/common/header/header-page.component";
 import {AppMenuComponent} from "../../views/common/header/app-menu.component";
 import {WarningDialogComponent} from "../../views/common/dialogs/warning-dialog.component";
+import {GrowlDialogComponent} from "../../views/common/dialogs/growl-dialog.component";
 import {HelpComponent} from "../../views/help/help.component";
 import {SettingsComponent} from "../../views/settings/settings.component";
 
 import {IonicModule} from "ionic-angular";
 
 @NgModule({
-    declarations: [HeaderPageComponent, RemoveParentElementDirective, AppMenuComponent, WarningDialogComponent, 
+    declarations: [HeaderPageComponent, RemoveParentElementDirective, AppMenuComponent, WarningDialogComponent, GrowlDialogComponent,
     HelpComponent, SettingsComponent],
     imports: [IonicModule],
     exports: [HeaderPageComponent, RemoveParentElementDirective],
-    entryComponents: [AppMenuComponent, WarningDialogComponent, HelpComponent, SettingsComponent]
+    entryComponents: [AppMenuComponent, WarningDialogComponent, GrowlDialogComponent, HelpComponent, SettingsComponent]
 })
 export class CommonDirectivesModule{}

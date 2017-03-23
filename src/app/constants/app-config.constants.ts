610e5d069f274075828d5eba7a54965b9166b663
Mike Campbell <michael.e.campbell82.ctr@mail.mil>
2017-03-23 09:29:21 -0400

    Modified current dev and test server list

--- src/app/constants/app-config.constants.ts
+++ src/app/constants/app-config.constants.ts
@@ -8,15 +8,12 @@ export class AppConfigConstants {
     public static clientLogFileName: string = "client.log";
 
     public static apiHosts: any = {
-        //"btBaseUrl"   : "http://jw8cui04:8080/"  // Test
-        //"btBaseUrl"   : "http://140.139.224.44:8080/"  // Test
+        //"btBaseUrl"   : "http://jw8cui04:8080/"  // OLD Test
 
-        //"btBaseUrl"   : "http://jw8dmles102:8080/"  // Dev Old
-        //"btBaseUrl"   : "http://140.139.227.170:8080/"  // Dev Old - jw8dmles103 (DON'T USE)
-        //"btBaseUrl"   : "http://140.139.227.169:8080/"  // Dev Old - jw9dmles102 (USE)
+        // From command prompt:
+        //   > set HOST=dmedpx3005.jmlfdc.mil      // Dev - NEW
+        //   > set HOST=dmetpx4007.jmlfdc.mil      // Test - NEW
 
-
-        //"btBaseUrl"   : "https:/140.139.35.29:8443/"  // Dev New
         "btBaseUrl": (AppConfigConstants.host)?`http://${AppConfigConstants.host}:8080/`:`http://localhost:8080/`
     };
 

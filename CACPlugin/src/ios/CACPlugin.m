ceb676b61003b5c461f5f0ade1a294efc5f5d0b7
John Tsinonis <jtsinoni@comcast.net>
2017-03-27 13:19:58 -0400

    DSE-890, continued with PKard integration

--- CACPlugin/src/ios/CACPlugin.m
+++ CACPlugin/src/ios/CACPlugin.m
@@ -17,20 +17,20 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-    
+
     if (PKardSDK_IsInstalled()) {
         PKardSDK_Initialize(NO);
-        
+
         // log all debug messages (7)
         TSSLogLevelSet(7);
-        
+
         PKardSDK_RegisterHTTPS();
-        
+
         // Allows PKard SDK to put up user interface dialogs or views such as the PIN entry view
         PKardSDK_MayDisplayUserInterface(YES);
     } else {
         // Show alert in cordova that the  "PKard Reader not installed"
-        
+
     }
 
     if (echo != nil && [echo length] > 0) {

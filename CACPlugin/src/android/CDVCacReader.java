1a2ae2b4c7a36530d17b737cb1eddf46b22e9109
John Tsinonis <jtsinoni@comcast.net>
2017-03-31 18:39:17 -0400

    DSE-890, fixed compile bug for CAC Android

--- CACPlugin/src/android/CDVCacReader.java
+++ CACPlugin/src/android/CDVCacReader.java
@@ -4,6 +4,8 @@ import org.apache.cordova.LOG;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
 import org.json.JSONException;

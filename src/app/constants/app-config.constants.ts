dce95add885b5c052bd47d10ca96b3a39479bac7
John Tsinonis <jtsinoni@comcast.net>
2017-06-07 15:30:16 -0400

    DSE-1388: added lock screen grahic

--- src/app/constants/app-config.constants.ts
+++ src/app/constants/app-config.constants.ts
@@ -28,8 +28,10 @@ export class AppConfigConstants {
     }
 
     public static messagingServer: any = {
-        host:"johns-macbook-pro.local",
-        // host:"192.168.1.129",
+        //host:"johns-macbook-pro.local",
+        host:"192.168.1.13",
+        //host:"192.168.1.129",
+        //host:"172.16.32.13",
         port:"9001",
         default:{topic:"dmles-mobile"},
         im:{topic:"dmles-mobile-im"},

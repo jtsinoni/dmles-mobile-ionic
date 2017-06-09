1a085a8b1151e78da7c041018c65d9755b592642
Paul <paul.w.edlund.ctr@mail.mil>
2017-06-09 13:52:44 -0400

    DSE-1403: Add and display meta data for ABi elastic search result

--- src/app/models/abi-catalog-result.model.ts
+++ src/app/models/abi-catalog-result.model.ts
@@ -1,11 +1,11 @@
 import { BaseResultModel } from './base-result.model';
 import { ABiCatalogModel } from './abi-catalog.model';
 
-export class ABiCatalogResultModel extends BaseResultModel {
+export class ABiCatalogResultModel extends BaseResultModel<ABiCatalogModel> {
 
-    public items: Array<ABiCatalogModel>; 
     constructor() {
         super();
         this.items = new Array();
+        this.resultCount = 0;
     }
 }
--- src/app/models/base-result.model.ts
+++ src/app/models/base-result.model.ts
@@ -1,6 +1,22 @@
-export class BaseResultModel {
+export class BaseResultModel<T> {
   resultCount: number;
   milliseconds: number;
-  hits: any;
+  resultReturned: boolean;
+  items: Array<T>;
+  //hits: any;
+
+  setDefaults() {
+    this.resultCount = 0;
+    this.milliseconds = 0;
+    this.resultReturned = false;
+  }
+
+  setResults(count: number, mSeconds: number, values: Array<T>) {
+    this.resultCount = count;
+    this.milliseconds = mSeconds;
+    this.items = values;
+  }
   
 }
+
+

92511d609e3c5aea5ffcb0b79f54542f63e7fd1c
John Tsinonis <jtsinoni@comcast.net>
2017-06-16 11:49:53 -0400

    DSE-1715: added native localnotification

--- src/app/constants/app-config.constants.ts
+++ src/app/constants/app-config.constants.ts
@@ -29,14 +29,14 @@ export class AppConfigConstants {
 
     public static messagingServer: any = {
         //host:"johns-macbook-pro.local",
-        //host:"192.168.1.13",
-        host:"192.168.1.129",
+        host:"192.168.1.13",
+        //host:"192.168.1.129",
         //host:"172.16.32.13",
         port:"9001",
-        default:{topic:"dmles-mobile"},
-        im:{topic:"dmles-mobile-im"},
-        eq:{topic:"dmles-mobile-eq"},
-        abi:{topic:"dmles-mobile-abi"},
+        default:{topic:"logicole-mobile"},
+        im:{topic:"logicole-mobile-im"},
+        eq:{topic:"logicole-mobile-eq"},
+        abi:{topic:"logicole-mobile-abi"},
         protocol:"ws",
         reconnectAttempts:3,
         reconnectPeriod:2000,
@@ -45,6 +45,10 @@ export class AppConfigConstants {
         connectionTimeout:3000,
         qos:0,
         showStats: true
+    }
+
+    public static notifications: any = {
+        enabled: true
     }
 
     public static topicComponent: any = {

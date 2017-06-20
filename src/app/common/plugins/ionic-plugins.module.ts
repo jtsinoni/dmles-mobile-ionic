e2b475703f32aa0a91724ac7f7da46525ab53c6f
John Tsinonis <jtsinoni@comcast.net>
2017-06-20 16:46:28 -0400

    DSE-1715: added ionic-native network

--- src/app/common/plugins/ionic-plugins.module.ts
+++ src/app/common/plugins/ionic-plugins.module.ts
@@ -1,6 +1,7 @@
 import {NgModule, ModuleWithProviders} from "@angular/core";
 import {BarcodeScanner} from "@ionic-native/barcode-scanner";
 import {LocalNotifications} from "@ionic-native/local-notifications";
+import {Network} from "@ionic-native/network";
 
 @NgModule({
 })
@@ -8,7 +9,7 @@ export class IonicPluginsModule {
     static forRoot():ModuleWithProviders {
         return {
             ngModule: IonicPluginsModule,
-            providers: [BarcodeScanner, LocalNotifications]
+            providers: [BarcodeScanner, LocalNotifications, Network]
         };
     }
 

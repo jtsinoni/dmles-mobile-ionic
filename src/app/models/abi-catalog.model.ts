fb598ed528a31666dd59c8aa3731abf9df51b7c2
Paul <paul.w.edlund.ctr@mail.mil>
2017-06-26 13:15:50 -0400

    DSE-1415: update search result to include preferred item

--- src/app/models/abi-catalog.model.ts
+++ src/app/models/abi-catalog.model.ts
@@ -22,11 +22,18 @@ export class ABiCatalogModel extends BaseDataTableModel {
     packaging:Array<any>;
     // test input value, not an ABi data element
     quantity:number;
+    preferredProductIndicator:string;
+    isPreferredItem: boolean;
 
     
     constructor(id?: number) {
         super(id);
         this.quantity = 0;
+        this.setPreferredItem();
+    }
+
+    setPreferredItem()  {
+        this.isPreferredItem = (this.preferredProductIndicator.toUpperCase() === "Y");
     }
 
 }
